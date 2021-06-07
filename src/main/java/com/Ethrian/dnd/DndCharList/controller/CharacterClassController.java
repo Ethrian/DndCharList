536fd8c1accd2b8875e790b1f7391b67dadfcb77
Ethrian <deniskrizhanovskiy2112@gmail.com>
2021-06-08 02:23:05 +0300

    spells & items functional

--- src/main/java/com/Ethrian/dnd/DndCharList/controller/CharacterClassController.java
+++ src/main/java/com/Ethrian/dnd/DndCharList/controller/CharacterClassController.java
@@ -27,18 +27,18 @@ public class CharacterClassController {
         return "/charClasses";
     }
 
-//    @GetMapping(name = "/{classId}")
-//    public String editClass(@PathVariable("classId") Long classId, @PathVariable("userId") Long userId, Map<String, Object> model){
-//        CharacterClass characterClass = characterClassService.getCharacterClass(classId);
-//        model.put("characterClass", characterClass);
-//        return "/editClass";
-//    }
+    @GetMapping(name = "/{classId}")
+    public String editClass(@PathVariable("classId") Long classId, @PathVariable("userId") Long userId, Map<String, Object> model){
+        CharacterClass characterClass = characterClassService.getCharacterClass(classId);
+        model.put("characterClass", characterClass);
+        return "/editClass";
+    }
 
-//    @GetMapping(name = "/delete")
-//    public String deleteClass(@PathVariable Long id, Map<String, Object> model){
-//        characterClassService.deleteCharacterClass(id);
-//        return "redirect:/charClasses";
-//    }
+    @DeleteMapping(name = "/delete")
+    public String deleteClass(@PathVariable Long id, Map<String, Object> model){
+        characterClassService.deleteCharacterClass(id);
+        return "redirect:/charClasses";
+    }
 
 
     @PostMapping(value = "/{id}")
